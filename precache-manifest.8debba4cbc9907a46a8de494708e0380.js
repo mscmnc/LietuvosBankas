ba9efab41bacd9109865d30ffcae3a2f165078a3
mscmnc <aurimas.zaleckas@gmail.com>
2019-02-10 22:16:19 +0200

    Updates

---
+++ precache-manifest.8debba4cbc9907a46a8de494708e0380.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c7ba2c2d41f5e70215b3",
+    "url": "/mscmnc/lietuvosbankas/static/css/main.df9c7db2.chunk.css"
+  },
+  {
+    "revision": "c7ba2c2d41f5e70215b3",
+    "url": "/mscmnc/lietuvosbankas/static/js/main.c7ba2c2d.chunk.js"
+  },
+  {
+    "revision": "596a9269423cadb87d3a",
+    "url": "/mscmnc/lietuvosbankas/static/css/1.bb42a244.chunk.css"
+  },
+  {
+    "revision": "596a9269423cadb87d3a",
+    "url": "/mscmnc/lietuvosbankas/static/js/1.596a9269.chunk.js"
+  },
+  {
+    "revision": "f90a79c15e364ddbdf4e",
+    "url": "/mscmnc/lietuvosbankas/static/js/runtime~main.f90a79c1.js"
+  },
+  {
+    "revision": "d48c4ba723cdbeb9f2ddbd1e961724b1",
+    "url": "/mscmnc/lietuvosbankas/index.html"
+  }
+];
